71b5e95222149240cbb3785e30802d9968e2f3ce
Caleb <quantumpandaprog@gmail.com>
2026-08-16 14:08:14 +0300

    fix(ping): validate command name and prefix

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,17 @@
+import { parseCommand } from "@/lib/commands.js";
 import { defineCommand } from "@/lib/eventHandler.js";
+import { getConfig } from "@/lib/getConfig.js";
 
 export default defineCommand(async (ctx) => {
-  await ctx.octokit.rest.issues.createComment(ctx.issue({ body: "Pong! " }));
+  const config = await getConfig(ctx);
+  const command = await parseCommand(ctx);
+
+  if (
+    command?.command !== "ping" ||
+    !ctx.payload.comment.body?.startsWith(config.settings.commands.prefix)
+  ) {
+    return;
+  }
+
+  await ctx.octokit.rest.issues.createComment(ctx.issue({ body: "Pong!" }));
 });
